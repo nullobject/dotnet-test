fb9c88d998568d2d2a3e5f64abe1350c68882fc7
Joshua Bassett <hello@joshbassett.info>
2025-03-28 23:25:55 +1100

    Use logger

--- Program.cs
+++ Program.cs
@@ -1,26 +1,14 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-public interface IMessageWriter
-{
-    void Write(string message);
-}
-
-public class MessageWriter : IMessageWriter
-{
-    public void Write(string message)
-    {
-        Console.WriteLine($"MessageWriter.Write(message: \"{message}\")");
-    }
-}
-
-public class Worker(IMessageWriter messageWriter) : BackgroundService
+public sealed class Worker(ILogger<Worker> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            messageWriter.Write($"Worker running at: {DateTimeOffset.Now}");
+            logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             await Task.Delay(1_000, stoppingToken);
         }
     }
@@ -33,7 +21,6 @@ class App
         HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
         builder.Services.AddHostedService<Worker>();
-        builder.Services.AddSingleton<IMessageWriter, MessageWriter>();
 
         using IHost host = builder.Build();
 
