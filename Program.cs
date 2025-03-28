a785b1b2655ea026d1d9baa9187fe5de704ecaf0
Joshua Bassett <hello@joshbassett.info>
2025-03-28 23:17:57 +1100

    Initial import

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public interface IMessageWriter
+{
+    void Write(string message);
+}
+
+public class MessageWriter : IMessageWriter
+{
+    public void Write(string message)
+    {
+        Console.WriteLine($"MessageWriter.Write(message: \"{message}\")");
+    }
+}
+
+public class Worker : BackgroundService
+{
+    private readonly MessageWriter _messageWriter = new();
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _messageWriter.Write($"Worker running at: {DateTimeOffset.Now}");
+            await Task.Delay(1_000, stoppingToken);
+        }
+    }
+}
+
+class App
+{
+    static void Main(string[]? args)
+    {
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+        builder.Services.AddHostedService<Worker>();
+        builder.Services.AddSingleton<IMessageWriter, MessageWriter>();
+
+        using IHost host = builder.Build();
+
+        host.Run();
+    }
+}
